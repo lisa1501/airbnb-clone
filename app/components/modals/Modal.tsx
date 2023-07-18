27526e2f482b793586e2d00b0bbde3db07eb4d45
Lisa S <halisasaipulla@gmail.com>
2023-07-18 11:50:16 -0700

    created Button component

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState, useEffect, useCallback } from "react";
 import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 
 interface ModalProps {
     isOpen?: boolean;
@@ -169,10 +170,14 @@ const Modal: React.FC<ModalProps> = ({
                                         w-full
                                     "
                                 >
-                                    {/* <Button /> */}
+                                    
+                                    <Button 
+                                        label="My button"
+                                    />
                                 </div>
                                 {footer}
                             </div>
+
                         </div>
                     </div>
                 </div>
