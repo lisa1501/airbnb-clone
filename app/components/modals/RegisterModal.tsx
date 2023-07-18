13addc763650fb61d6f50186cd0c9c9a99aedfee
Lisa S <halisasaipulla@gmail.com>
2023-07-18 15:11:44 -0700

    rendered footercontent

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,19 +1,19 @@
 'use client'
 import axios from 'axios';
 import { FcGoogle } from 'react-icons/fc';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import {
     FieldValues,
     SubmitHandler,
     useForm
 } from 'react-hook-form';
 
-
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal'
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast';
+import Button from "../Button";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -83,7 +83,47 @@ const RegisterModal = () => {
         </div>
     )
 
-    
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button 
+                outline 
+                label="Continue with Google"
+                icon={FcGoogle}
+                onClick={() => {}}
+            />
+            <div className="
+                    text-neutral-500 
+                    text-center 
+                    mt-4 
+                    font-light
+                "
+            >
+                <div className='
+                        justify-cneter 
+                        flex-row 
+                        items-center 
+                        gap-2
+                    '
+                >
+                    <div>
+                        Already have an account?
+                    </div>
+                    
+                    <div 
+                        onClick={registerModal.onClose}
+                        className='
+                            text-neutral-800 
+                            cursor-pointer 
+                            hover:underline
+                        '
+                    >
+                        Log in
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 
     return ( 
         <Modal 
@@ -94,6 +134,7 @@ const RegisterModal = () => {
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
         />
     );
 }
