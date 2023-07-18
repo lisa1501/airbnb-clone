dbd9fbcd576f84f2db750ab2e79985630c428933
Lisa S <halisasaipulla@gmail.com>
2023-07-18 14:03:28 -0700

    rendered register modal component and user register submit function

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,8 +3,10 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 import { useCallback, useState } from "react";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
+    const registerModal = useRegisterModal()
     const [isOpen, setIsOpen] = useState(false)
     const toggleOpen = useCallback(() =>{
         setIsOpen((value) => !value);
@@ -76,7 +78,7 @@ const UserMenu = () => {
                             label="Login"
                         />
                         <MenuItem 
-                            onClick={() =>{}}
+                            onClick={registerModal.onOpen}
                             label="Sign up"
                         />
                     </>
