e04b6326d8b9a964150eeb1740834149d5c70e05
Lisa S <halisasaipulla@gmail.com>
2023-07-18 14:10:56 -0700

    added Heading component for RegisterModal bodycontent

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import {
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal'
+import Heading from '../Heading';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -45,6 +46,15 @@ const RegisterModal = () => {
             })
     }
 
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Heading
+                title="Welcome to Airbnb"
+                subtitle="Create an account!"
+            />
+        </div>
+    )
+
     return ( 
         <Modal 
             disabled={isLoading}
@@ -53,6 +63,7 @@ const RegisterModal = () => {
             actionLabel="Continue"
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
         />
     );
 }
