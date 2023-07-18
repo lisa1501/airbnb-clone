17f26c770dfc9d72d8daf91c982f06552095bc33
Lisa S <halisasaipulla@gmail.com>
2023-07-18 14:29:13 -0700

    rendered user signup form

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -12,6 +12,7 @@ import {
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal'
 import Heading from '../Heading';
+import Input from '../inputs/Input';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -51,6 +52,32 @@ const RegisterModal = () => {
             <Heading
                 title="Welcome to Airbnb"
                 subtitle="Create an account!"
+            />
+
+            <Input 
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input 
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input 
+                id="password"
+                type="password"
+                label="Password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
             />
         </div>
     )
