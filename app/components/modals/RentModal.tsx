303cbb7570a971163c0585edb235faf2dd7567fe
Lisa S <halisasaipulla@gmail.com>
2023-07-22 10:22:55 -0700

    added all world countries hook

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,6 +6,7 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Catogries";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
     CATEGORY = 0,
@@ -112,6 +113,7 @@ const RentModal = () => {
                     title="Where is your place located?"
                     subtitle="Help guests find you!"
                 />
+                <CountrySelect />
             </div>
         )
     }
