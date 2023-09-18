a8ded854922dfc8dd4436a117330b956982f3e5b
Lisa S <halisasaipulla@gmail.com>
2023-09-18 16:03:18 -0700

    Loading trips with server component

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -54,10 +54,12 @@ const ListingCard: React.FC<ListingCardProps> = ({
             return null;
         }
 
+        
+
         const start = new Date(reservation.startDate);
         const end = new Date(reservation.endDate);
         
-        return `${format(start,'pp')} - ${format(end,'pp')}`
+        return `${format(start,'PP')} - ${format(end,'PP')}`
     },[reservation]);
     return ( 
         <div
