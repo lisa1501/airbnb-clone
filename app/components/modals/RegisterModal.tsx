a392bc5fcbee6bd95a07a9c44be23a4d016c46f1
Lisa S <halisasaipulla@gmail.com>
2023-09-19 11:12:33 -0700

    Loading guest reservations with server componen

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -42,6 +42,7 @@ const RegisterModal = () => {
         axios.post('/api/register',data)
             .then(()=>{
                 registerModal.onClose();
+                loginModal.onOpen();
             })
             .catch((error)=>{
                 toast.error('Something went wrong !')
